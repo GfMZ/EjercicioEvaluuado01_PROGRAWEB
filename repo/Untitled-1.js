15e6d57bf7258ffaf5e9a1fc45491fe85af96a07
Gianfranco <20236441@aloe.ulima.edu.pe>
2025-09-13 12:24:16 -0500

    Add files via upload

---
+++ repo/Untitled-1.js
@@ -0,0 +1,6 @@
+function temperatura(){
+    var F = document.getElementById('temp').value
+}
+
+const resultado = (F-32)*5/9
+alert("La temperatura es: ", document.getElementById('resultado').innerHTML = resultado);
